4edea5470392bb661415f18dcb01d39334e7fefd
segave <sergiogalianavelasco@gmail.com>
2021-11-24 20:09:39 +0100

    actualización recursos humanos

--- rrhh/models.py
+++ rrhh/models.py
@@ -4,17 +4,14 @@ from django.db import models
 
 class OfertaEmpleo(models.Model):
     IDOfertaEmpleo=models.CharField(max_length=9, primary_key=True)
-    ListaEmpleos=models.CharField(max_length=100)
-    FechaInicio=models.DateField()
-    FechaFin=models.DateField()
+    ListadoEmpleos=models.CharField(max_length=100)
+    FechaIni_OfertEmp=models.DateField()
+    FechaFin_OfertEmp=models.DateField()
 
 class Contrato(models.Model):
     DNI=models.CharField(max_length=9, primary_key=True)
     IDOfertaEmpleo=models.ForeignKey(OfertaEmpleo, on_delete=models.CASCADE , max_length=9)
-    NombreEmpleado=models.CharField(max_length=30)
-    Telefono=models.IntegerField(max_length=9)
-    NumeroSeguridadSocial=models.IntegerField(max_length=12)
-    Salario=models.DecimalField(decimal_places=2)
-
-
-
+    Nombre_Empleado=models.CharField(max_length=30)
+    Tlf_Empleado=models.IntegerField(max_length=9)
+    NumSegSocial=models.IntegerField(max_length=12)
+    Salario=models.DecimalField(decimal_places=2, max_digits=10)
--- rrhh/views.py
+++ rrhh/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+#from .forms import *
+import cx_Oracle
+
+# Create your views here.
+
+def menu_rrhh(request):
+    if request.method == 'POST':
+        keys_request_POST = request.POST.keys()
+            # Obtengo las claves del diccionario request.POST, que por ejemplo tiene el valor:
+            # <QueryDict: {'csrfmiddlewaretoken': ['CHNZtCiLbU'], 'alamcen-btn': ['']}> 
+            # Me quedo con las claves para coger "almacen-btn", que es la que me interesa 
+            # coger (según el boton que pulse y se escoja hacer una acción u otra)
+        if 'alta_contrato-btn' in keys_request_POST:
+            pass # se pone cuando aun falta por implementar
+            # return HttpResponseRedirect("menu/almacen")
+        elif 'contabilidad-tablas-btn' in keys_request_POST:
+            pass
+            # return HttpResponseRedirect("menu/contabilidad")
+        elif 'logistica-btn' in keys_request_POST:
+            pass
+            # return HttpResponseRedirect("menu/logistica")
+        elif 'marketing-btn' in keys_request_POST:
+            return HttpResponseRedirect("marketing")
+        elif 'rrhh-btn' in keys_request_POST:
+            pass
+            # return HttpResponseRedirect("menu/rrhh")
+    
+    return render(request,"menu_rrhh.html")
